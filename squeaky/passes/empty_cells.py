80dc4bf5094cc4190a1bfabebcab6bb4df5a3813
Frank Harkins <frankharkins@hotmail.co.uk>
2023-09-28 16:26:01 +0100

    feat: ignore default metadata values when deciding if cell is empty

--- squeaky/passes/empty_cells.py
+++ squeaky/passes/empty_cells.py
@@ -1,3 +1,24 @@
+def _has_metadata(cell):
+    if not cell.metadata:
+        return False
+
+    defaults = {
+        "editable": True,
+        "slideshow": {
+         "slide_type": ""
+        },
+        "tags": []
+    }
+
+    metadata = dict(cell.metadata)
+    for key, value in defaults.items():
+        if metadata.pop(key, value) != value:
+            return True
+
+    if not metadata:
+        return False
+    return True
+
 def clean_empty_cells(notebook):
     """
     Remove any empty cells (source is empty string, or metadata is empty dict).
@@ -5,7 +26,7 @@ def clean_empty_cells(notebook):
     initial_len = len(notebook.cells)
     notebook.cells[:] = [
         cell for cell in notebook.cells
-        if cell.source or cell.metadata
+        if cell.source or _has_metadata(cell)
     ]
 
     if len(notebook.cells) != initial_len:
